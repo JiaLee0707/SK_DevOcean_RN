9f8c2a22b64d08fa8c8780ce1824b77adb55c29d
JiaLee <s2018s37@e-mirim.hs.kr>
2023-04-16 02:16:07 +0900

    📝2.5.1 Props로 컴포넌트 스타일을 커스터마이징하기

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
+// import Greeting from './components/Greeting';
+import Box from './components/Box';
 
 const App = () => {
+  // const name = 'JSX';
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
+      {/* 주석을 작성해봅시다. */}
+      {/* <Greeting
+        name={name} // 이름을 설정하기
+      /> */}
+      <Box rounded={true} size="large" color="blue" />
     </SafeAreaView>
   );
 };
