d5f83829918ac000d881dd2d1b3cbedbcb7af340
JiaLee <s2018s37@e-mirim.hs.kr>
2023-04-22 18:21:30 +0900

    📚4장 할일 목록 만들기 II

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
 import TodoList from './components/TodoList';
-
+import todosStorage from './storages/todosStorage';
 
 function App() {
   const today = new Date();
@@ -16,6 +18,65 @@ function App() {
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
 
+  // 불러오기
+  useEffect(() => {
+    // async function load() {
+    //   try {
+    //     const rawTodos = await AsyncStorage.getItem('todos');
+    //     const savedTodos = JSON.parse(rawTodos);
+    //     setTodos(savedTodos);
+    //   } catch (e) {
+    //     console.log('Failed to load todos');
+    //   }
+    // }
+    // load();
+    todosStorage
+      .get()
+      .then(setTodos)
+      .catch(console.error);
+  }, []);
+
+  // 저장
+  useEffect(() => {
+    // async function save() {
+    //   try {
+    //     await AsyncStorage.setItem('todos', JSON.stringify(todos));
+    //   } catch (e) {
+    //     console.log('Failed to save todos');
+    //   }
+    // }
+    // save();
+    todosStorage.set(todos).catch(console.error);
+  }, [todos]);
+
+  const onInsert = text => {
+    // 새로 등록할 항목의 id를 구합니다.
+    // 등록된 항목 중에서 가장 큰 id를 구하고, 그 값에 1을 더합니다.
+    // 만약 리스트가 비어있다면 1을 id로 사용합니다.
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+      
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+    setTodos(nextTodos);
+  };
+
+  const onRemove = id => {
+    const nextTodos = todos.filter(todo => todo.id !== id);
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -23,8 +84,12 @@ function App() {
           behavior={Platform.select({ios: 'padding'})}
           style={styles.avoid}>
             <DateHead date={today}/>
-            {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
-            <AddTodo />
+            {todos.length === 0 ? (
+              <Empty />
+            ) : (
+              <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
+            )}
+            <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
